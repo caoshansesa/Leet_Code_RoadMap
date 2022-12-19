8a441fb65b8efcaeabdcb01e91faf27034f1326c
“Shan <cao.shan2@northeastern.edu>
2022-12-19 03:21:13 +0000

    Add first code

---
+++ 01_hungury_kid/hungury_kid.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int max(int *Array)
+{
+    return 0;
+}
+
+
+int main()
+{
+    int kid_a[] = {1, 2, 3, 4, 5, 6, 7};
+    int cake_arr[] = {4, 3, 2, 5, 4, 6, 8};
+
+    // find the most hungury kid
+    int max_hungury_kid;
+    return 0;
+}
