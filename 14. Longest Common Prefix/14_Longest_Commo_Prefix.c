405c3691bb095b86cc3bc01ec88afaf73fe40634
Shan <shan.cao@lowell.church>
2023-11-21 12:18:57 -0500

    update

--- 14. Longest Common Prefix/14_Longest_Commo_Prefix.c
+++ 14. Longest Common Prefix/14_Longest_Commo_Prefix.c
@@ -5,23 +5,32 @@ char* longestCommonPrefix(char** strs, int strsSize) {
     if (strsSize == 0) {
         return "";
     }
-    for (int i = 1; i < strsSize; i++) {
-        int j = 0;
-        strs[0][j] = strs[0][j] == strs[i][j] ? strs[0][j] : '\0';
+
+    // Find the length of the first string
+    int len = strlen(strs[0]);
+
+    for (int j = 0; j < len; j++) {
+        for (int i = 1; i < strsSize; i++) {
+            if (strs[i][j] != strs[0][j]) {
+                strs[0][j] = '\0'; // Null-terminate the common prefix
+                return strs[0];
+            }
+        }
     }
+
     return strs[0];
 }
-int main() {
-    // the string already been stack as 
-    //strs = ["flower","flow","flight"]
-    //str[0] = "flower"
-    //str[1] = "flow"
-    //STR[2] = "flight"
 
+int main() {
     char* strs[] = {"flower", "flow", "flight"};
-    int strs:q
-    Size = 3;
+    int strsSize = 3;
     char* result = longestCommonPrefix(strs, strsSize);
-    printf("Longest Common Prefix: %s\n", result);
+
+    if (strlen(result) == 0) {
+        printf("No common prefix found.\n");
+    } else {
+        printf("Longest Common Prefix: %s\n", result);
+    }
+
     return 0;
 }
