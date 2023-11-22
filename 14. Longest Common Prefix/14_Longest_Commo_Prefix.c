8bff61807eda8734e53cb403164ede2a558c94d0
Shan <shan.cao@lowell.church>
2023-11-22 12:21:10 -0500

    update for code, Sign10 error fixed

--- 14. Longest Common Prefix/14_Longest_Commo_Prefix.c
+++ 14. Longest Common Prefix/14_Longest_Commo_Prefix.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 char* longestCommonPrefix(char** strs, int strsSize) {
     if (strsSize == 0) {
@@ -8,17 +9,22 @@ char* longestCommonPrefix(char** strs, int strsSize) {
 
     // Find the length of the first string
     int len = strlen(strs[0]);
+    
+    // We must use the malloced memory, the char array will not allow add NULL Null-terminate, will issue a SIG 10 error
+    char* prefix = (char*)malloc((len + 1) * sizeof(char)); // Allocate memory for the common prefix
 
     for (int j = 0; j < len; j++) {
-        for (int i = 1; i < strsSize; i++) {
+        for (int i = 0; i < strsSize; i++) {
             if (strs[i][j] != strs[0][j]) {
-                strs[0][j] = '\0'; // Null-terminate the common prefix
-                return strs[0];
+                prefix[j] = '\0'; // Null-terminate the common prefix
+                return prefix;
             }
         }
+        prefix[j] = strs[0][j]; // Copy character to common prefix
     }
 
-    return strs[0];
+    prefix[len] = '\0'; // Null-terminate the common prefix
+    return prefix;
 }
 
 int main() {
@@ -31,6 +37,8 @@ int main() {
     } else {
         printf("Longest Common Prefix: %s\n", result);
     }
+
+    free(result); // Don't forget to free the allocated memory
 
     return 0;
 }
