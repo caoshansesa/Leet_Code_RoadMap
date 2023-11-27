553d7cda09277cddcb11a35504aa92d75efeb3ff
Shan Cao <Shan.Cao@lowell.church>
2023-11-27 15:55:01 -0500

    update formate

--- 03_hungury_kid/hungry.c
+++ 03_hungury_kid/hungry.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int compare(const void * a, const void * b) {
-  return (*(int*)a - *(int*)b);
+int compare(const void *a, const void *b)
+{
+    return (*(int *)a - *(int *)b);
 }
 
-int findContentChildren(int* g, int gSize, int* s, int sSize){
+int findContentChildren(int *g, int gSize, int *s, int sSize)
+{
     qsort(g, gSize, sizeof(int), compare);
     qsort(s, sSize, sizeof(int), compare);
 
     int child = 0;
-    for (int i = 0; i < sSize && child < gSize; i++) {
-        if (g[child] <= s[i]) {
+    for (int i = 0; i < sSize && child < gSize; i++)
+    {
+        if (g[child] <= s[i])
+        {
             child++;
         }
     }
@@ -19,11 +23,13 @@ int findContentChildren(int* g, int gSize, int* s, int sSize){
 }
 
 // Test the function
-int main() {
-    int greed[] = {1, 2, 3}; // Greed factors of children
+int main()
+{
+    int greed[] = {1, 2, 3};    // Greed factors of children
     int cookieSizes[] = {1, 1}; // Sizes of cookies
 
-    int numKids = findContentChildren(greed, sizeof(greed) / sizeof(greed[0]), cookieSizes, sizeof(cookieSizes) / sizeof(cookieSizes[0]));
+    int numKids = findContentChildren(greed, sizeof(greed) / sizeof(greed[0]), cookieSizes,
+                                      sizeof(cookieSizes) / sizeof(cookieSizes[0]));
 
     printf("Number of content children: %d\n", numKids);
     return 0;
