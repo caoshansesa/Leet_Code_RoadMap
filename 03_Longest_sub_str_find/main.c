32e97498e384b48b11ac5857b5328728d921fac0
Shan Cao <shan.cao@lowell.church>
2023-12-13 16:50:44 +0000

    Add new leet code

---
+++ 03_Longest_sub_str_find/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+double fmax(double a, double b)
+{
+    return a > b ? a : b;
+}
+int lengthOfLongestSubstring(char *s)
+{
+    // Get length of the string.
+    int lengthOfStr = strlen(s);
+    // Declare the longest substring length.
+    int lengthOFlongestSubStr = 0;
+
+    int indexer = 0;
+    for (int i = 0; i < lengthOfStr; i++)
+    {
+        for (int j = i + 1; j < lengthOfStr; j++)
+        {
+            if (s[i] == s[j])
+            {
+                // only find longest str if there is no repeat, if there is repeat, break, 
+                // save the longest substring length. that is the longest substring length for current index
+                // Then loop through next one, and compare, only keep the longer length
+                lengthOFlongestSubStr = fmax(lengthOFlongestSubStr, j - i);
+                break;
+            }
+        }
+    }
+    return lengthOFlongestSubStr;
+}
+
+int main()
+{
+    char s[] = "abcabcbb";
+    int len = lengthOfLongestSubstring(s);
+    printf("%d\n", len);
+    return 0;
+}
